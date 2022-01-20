67b613c996ec74ef7df9ffd7b03f32c215299d0e
Rika Fonseca <fonsecarika@gmail.com>
2022-01-20 19:37:21 +0200

    routes to be tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  ressources :gardens do
+    ressources :plants, only: [:create]
+
+  end
+
+  ressources :plants, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
