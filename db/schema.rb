ab00562e128df5d3f8a2ef47c1402c4c774375d7
Rika Fonseca <fonsecarika@gmail.com>
2022-01-20 19:32:42 +0200

    model done db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_01_20_172546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "gardens", force: :cascade do |t|
+    t.string "name"
+    t.string "banner_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "plants", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.bigint "garden_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["garden_id"], name: "index_plants_on_garden_id"
+  end
+
+  add_foreign_key "plants", "gardens"
 end
