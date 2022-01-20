d6e1afde687135d7da7a6b5113289fa86f6b794b
Rika Fonseca <fonsecarika@gmail.com>
2022-01-20 19:38:48 +0200

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  ressources :gardens do
-    ressources :plants, only: [:create]
+  resources :gardens do
+    resources :plants, only: [:create]
 
   end
 
-  ressources :plants, only: [:destroy]
+  resources :plants, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
